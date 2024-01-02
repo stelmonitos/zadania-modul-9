4ab7ddbc27c787fe361ba7bc807479464e595b76
Jakub Stelmasiak <stelmon3@gmail.com>
2024-01-02 14:07:31 +0100

    exercise 2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const employees = {
+    john: {
+      name: 'John Doe',
+      salary: 3000
+    },
+    amanda: {
+      name: 'Amanda Doe',
+      salary: 4000
+    },
+  }
+  console.log(employees)
